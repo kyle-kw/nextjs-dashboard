3364edd95fc02b2d51b291d71912848c75506df7
kewei <huangkewei1998@gmail.com>
2024-09-23 22:15:25 +0800

    feature: add routes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,29 @@
-export default function Page(){
-  return <p>Customers Page</p>
+
+import CustomersTable from '@/app/ui/customers/table';
+import {fetchFilteredCustomers} from "@/app/lib/data";
+import {Suspense} from "react";
+import {InvoicesTableSkeleton} from "@/app/ui/skeletons";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+ searchParams
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className='w-full'>
+      <Suspense fallback={<InvoicesTableSkeleton/>}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  )
 }
